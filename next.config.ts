afc27a030305da5c01b5d2cae912c3e9cfd9629b
Victor Munyi <munyivictor6@gmail.com>
2025-09-27 13:56:40 +0300

    setup the kenya safaris wbesite and install the necessary dependancies

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type { NextConfig } from "next";
 
-import { resolve } from 'path';
-
 const nextConfig: NextConfig = {
-reactStrictMode: true,
-experimental: {
-appDir: true
-},
-images: {
-// Add any external hosts you plan to use for images
-remotePatterns: [
-{ protocol: 'https', hostname: '**.unsplash.com' },
-{ protocol: 'https', hostname: '**.images.unsplash.com' }
-]
-}
+  /* config options here */
 };
 
-
 export default nextConfig;
+
